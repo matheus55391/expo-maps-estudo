ae7367702e6f9b9ce91539b53db05e418fc3d035
matheus55391 <matheus.manulo55@gmail.com>
2023-12-22 09:45:05 -0300

    deletando pasta android

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,7 +1,7 @@
+import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 
 interface LocationObject {
   coords: {
